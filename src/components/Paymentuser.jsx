9f237045b627a19dda6d2a1d91036d5fd563ca5b
Voidwalker@99 <v.g.pasinduhasaranga@gmail.com>
2023-12-24 11:42:55 +0530

    Remove and Update files

---
+++ src/components/Paymentuser.jsx
@@ -0,0 +1,180 @@
+import React, { useState } from 'react';
+
+function Paymentuser() {
+    const [paymentMethod, setPaymentMethod] = useState('');
+    const [currency, setCurrency] = useState('');
+    const [cardType, setCardType] = useState('');
+    const [cardNumber, setCardNumber] = useState('');
+    const [cardHolderName, setCardHolderName] = useState('');
+    const [expirationDate, setExpirationDate] = useState('');
+    const [secretCode, setSecretCode] = useState('');
+    const [cardDetails, setCardDetails] = useState([]);
+    const [newCard, setNewCard] = useState('');
+
+    const handlePaymentMethodChange = (event) => {
+        setPaymentMethod(event.target.value);
+    };
+
+    const handleCurrencyChange = (event) => {
+        setCurrency(event.target.value);
+    };
+
+    const handleCardTypeChange = (event) => {
+        setCardType(event.target.value);
+    };
+
+    const handleCardNumberChange = (event) => {
+        setCardNumber(event.target.value);
+    };
+
+    const handleCardHolderNameChange = (event) => {
+        setCardHolderName(event.target.value);
+    };
+
+    const handleExpirationDateChange = (event) => {
+        setExpirationDate(event.target.value);
+    };
+
+    const handleSecretCodeChange = (event) => {
+        setSecretCode(event.target.value);
+    };
+
+    const handleCardDetailsChange = (event) => {
+        setNewCard(event.target.value);
+    };
+
+    const handleAddCard = () => {
+        const newCardDetails = {
+            cardType,
+            cardNumber,
+            cardHolderName,
+            expirationDate,
+            secretCode
+        };
+        setCardDetails([...cardDetails, newCardDetails]);
+        setCardType('');
+        setCardNumber('');
+        setCardHolderName('');
+        setExpirationDate('');
+        setSecretCode('');
+    };
+
+    const handleRemoveCard = (index) => {
+        const updatedCardDetails = [...cardDetails];
+        updatedCardDetails.splice(index, 1);
+        setCardDetails(updatedCardDetails);
+    };
+
+    return (
+        <div>
+            <h1>Payment Settings</h1>
+            <h3>Customize payment options</h3>
+            <div id="paymentMethodDiv">
+                <label htmlFor="paymentMethod">Payment Method:</label>
+                <select id="paymentMethod" value={paymentMethod} onChange={handlePaymentMethodChange}>
+                    <option value="">Select Payment Method</option>
+                    <option value="creditCard">Credit Card</option>
+                    <option value="paypal">PayPal</option>
+                    <option value="directBank">Direct Bank Transaction</option>
+                </select>
+            </div>
+
+            <div id="currencyDiv">
+                <label htmlFor="currency">Currency:</label>
+                <select id="currency" value={currency} onChange={handleCurrencyChange}>
+                    <option value="">Select Currency</option>
+                    <option value="usd">USD</option>
+                    <option value="eur">EUR</option>
+                </select>
+            </div>
+
+            {paymentMethod === 'creditCard' && (
+                <div id="cardTypeDiv">
+                    <label htmlFor="cardType">Card Type:</label>
+                    <select id="cardType" value={cardType} onChange={handleCardTypeChange}>
+                        <option value="">Select Card Type</option>
+                        <option value="credit">Credit Card</option>
+                        <option value="debit">Debit Card</option>
+                    </select>
+                </div>
+            )}
+
+            {paymentMethod === 'creditCard' && (
+                <div id="cardNumberDiv">
+                    <label htmlFor="cardNumber">Card Number:</label>
+                    <input id="cardNumber" type="text" value={cardNumber} onChange={handleCardNumberChange} />
+                </div>
+            )}
+
+            {paymentMethod === 'creditCard' && (
+                <div id="cardHolderNameDiv">
+                    <label htmlFor="cardHolderName">Card Holder Name:</label>
+                    <input id="cardHolderName" type="text" value={cardHolderName} onChange={handleCardHolderNameChange} />
+                </div>
+            )}
+
+            {paymentMethod === 'creditCard' && (
+                <div id="expirationDateDiv">
+                    <label htmlFor="expirationDate">Expiration Date:</label>
+                    <input id="expirationDate" type="text" value={expirationDate} onChange={handleExpirationDateChange} />
+                </div>
+            )}
+
+            {paymentMethod === 'creditCard' && (
+                <div id="secretCodeDiv">
+                    <label htmlFor="secretCode">Secret Code:</label>
+                    <input id="secretCode" type="text" value={secretCode} onChange={handleSecretCodeChange} />
+                </div>
+            )}
+
+            <div id="cardDetailsDiv">
+                <label htmlFor="cardDetails">Card Details:</label>
+                <div>
+                    <label htmlFor="cardNumber">Card Number:</label>
+                    <input id="cardNumber" type="text" value={newCard.cardNumber} onChange={handleCardDetailsChange} />
+                </div>
+                <div>
+                    <label htmlFor="cardHolderName">Card Holder Name:</label>
+                    <input id="cardHolderName" type="text" value={newCard.cardHolderName} onChange={handleCardDetailsChange} />
+                </div>
+                <div>
+                    <label htmlFor="expirationDate">Expiration Date:</label>
+                    <input id="expirationDate" type="text" value={newCard.expirationDate} onChange={handleCardDetailsChange} />
+                </div>
+                <div>
+                    <label htmlFor="secretCode">Secret Code:</label>
+                    <input id="secretCode" type="text" value={newCard.secretCode} onChange={handleCardDetailsChange} />
+                </div>
+                <button onClick={handleAddCard}>Add Card</button>
+            </div>
+
+            <div>
+                <h2>Saved Card Details:</h2>
+                {cardDetails.map((card, index) => (
+                    <div key={index}>
+                        <p>Card Type: {card.cardType}</p>
+                        <p>Card Number: {card.cardNumber}</p>
+                        <p>Card Holder Name: {card.cardHolderName}</p>
+                        <p>Expiration Date: {card.expirationDate}</p>
+                        <p>Secret Code: {card.secretCode}</p>
+                        <button onClick={() => handleRemoveCard(index)}>Remove</button>
+                    </div>
+                ))}
+
+                {/* Display entered card details */}
+                {newCard.cardNumber && (
+                    <div>
+                        <p>Card Type: {newCard.cardType}</p>
+                        <p>Card Number: {newCard.cardNumber}</p>
+                        <p>Card Holder Name: {newCard.cardHolderName}</p>
+                        <p>Expiration Date: {newCard.expirationDate}</p>
+                        <p>Secret Code: {newCard.secretCode}</p>
+                        <button onClick={handleRemoveCard}>Remove</button>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Paymentuser;
